1347b7933f6cc597cc67632fafbc1ab4bd33ad86
REI OKADA <rei@REInoMacBook-Pro.local>
2020-03-25 20:10:45 +0900

    [Finish]5-5

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
 
   root 'post_images#index'
 
-  resources :post_images, only: [:new, :create, :index, :show] do
-  	resource :favorites, only: [:create, :destroy]
-  	resource :post_comments, only: [:create]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resource :post_comments, only: [:create, :destroy]
   end
+
+  resources :users, only: [:show]
+
 end
